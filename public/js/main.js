77fb825d84e98ed766f4f6e2f486db96e32bda7e
Kishanpal Saini <kishanpalsaini@hotmail.com>
2020-07-29 22:54:55 +0530

    pagination and navbar,about breadcrumb

--- public/js/main.js
+++ public/js/main.js
@@ -126,7 +126,7 @@
 			} 
 			if (st < 150) {
 				if ( navbar.hasClass('scrolled') ) {
-					navbar.removeClass('scrolled sleep');
+					// navbar.removeClass('scrolled sleep');
 				}
 			} 
 			if ( st > 350 ) {
@@ -134,14 +134,14 @@
 					navbar.addClass('awake');	
 				}
 				
-				if(sd.length > 0) {
-					sd.addClass('sleep');
-				}
+				// if(sd.length > 0) {
+				// 	sd.addClass('sleep');
+				// }
 			}
 			if ( st < 350 ) {
 				if ( navbar.hasClass('awake') ) {
-					navbar.removeClass('awake');
-					navbar.addClass('sleep');
+					// navbar.removeClass('awake');
+					// navbar.addClass('sleep');
 				}
 				if(sd.length > 0) {
 					sd.removeClass('sleep');
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 @extends ('layouts.main')
 
-<div class="hero-wrap " style="background-image: url('images/bg_3.jpg');" data-stellar-background-ratio="0.5">
+<div class="hero-wrap d-flex align-items-center" style="background-image: url('images/bg_3.jpg');" data-stellar-background-ratio="0.5">
       <div class="overlay"></div>
       <div class="container">
         <div class="row no-gutters slider-text  align-items-center justify-content-start" data-scrollax-parent="true">
